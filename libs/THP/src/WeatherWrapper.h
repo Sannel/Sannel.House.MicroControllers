89a4eb69f2e4f79413144175e52d128c0e13f765
Adam Holt <holtsoftware@outlook.com>
2017-07-24 21:27:56 -0600

    Updates

--- libs/THP/src/WeatherWrapper.h
+++ libs/THP/src/WeatherWrapper.h
@@ -1,8 +1,22 @@
-﻿#if defined SparkFun_Photon_Weather_Shield_Library_h
+﻿/* Copyright 2017 Sannel Software, L.L.C.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.*/
+#ifdef SUPPORT_PARTICLE_SPARKFUN_WEATHER
 #ifndef _WEATHERWRAPPER_
 #define _WEATHERWRAPPER_
 
 #include "ITHPSensor.h"
+#include "SparkFun_Photon_Weather_Shield_Library.h"
 
 
 class WeatherWrapper : public ITHPSensor
