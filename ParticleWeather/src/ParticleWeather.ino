cb697111798f4d15295e74e81a958395b9acd124
Adam Holt <holtsoftware@outlook.com>
2017-07-26 22:16:49 -0600

    Have a working version for Photon weather shield

--- ParticleWeather/src/ParticleWeather.ino
+++ ParticleWeather/src/ParticleWeather.ino
@@ -5,25 +5,29 @@
  * Date:
  */
 #define DEBUG
-#include "SparkFun_Photon_Weather_Shield_Library.h"
 #include "SannelDefines.h"
-#include "Particle.h"
+#include "LocalDefines.h"
+#include "WeatherWrapper.h"
 #include "TemperatureSensorWrapper.h"
-#include "WeatherWrapper"
+#include "Particle.h"
 
+TCPClient client;
+WeatherWrapper wrapper;
+TemperatureSensorWrapper sensor(11, wrapper);
+IPAddress broadcast(BROADCASTIP_FIRST, BROADCASTIP_SECOND, BROADCASTIP_THIRD, BROADCASTIP_FORTH);
 
-Weather weather;
-WeatherWrapper wrapper(weather);
 
 // setup() runs once, when the device is first turned on.
 void setup() {
+    Particle.publish("started", "started");
 	// Put initialization like pinMode and begin functions here.
-    Particle.publish("test", SENSOR_BROADCAST_PORT);
-
+    sensor.initialize();
+    sensor.begin();
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
 	// The core of your code will likely live here.
-
+    sensor.prepareAndSendPacket(client, broadcast);
+	delay(900000);
 }
